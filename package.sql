9ed5833753688b284d0dfb64429454f1066f7d6c
Iarnaiz <78861772+Iarnaiz@users.noreply.github.com>
2022-04-05 10:44:09 +0200

    update package, procedure 1 and 2

--- package.sql
+++ package.sql
@@ -75,3 +75,72 @@ CREATE OR REPLACE PACKAGE BODY PKG_users AS
 END PKG_users;
 /
 
+
+
+
+--------------------------------------------------------------------------
+
+
+
+
+SET SERVEROUTPUT ON;
+
+CREATE OR REPLACE PACKAGE  package1  is
+   curr_user varchar2(15):= null;
+   PROCEDURE procedure1 (userid VARCHAR2);
+   PROCEDURE procedure2 (productname VARCHAR2, companyname VARCHAR2);
+   PROCEDURE procedure3 (schedule DATE, doctor VARCHAR2, hospital VARCHAR2);
+   END package1;
+/
+
+CREATE OR REPLACE PACKAGE BODY package1 is
+   -- Procedure1 body
+   PROCEDURE procedure1 (userid VARCHAR2) AS
+   exist number(2);
+   BEGIN
+     select count (*) into exist from clients where passport = procedure1.userid;
+     if exist = 1 then
+     package1.curr_user := procedure1.userid;
+     dbms_output.put_line ('User found');
+     else
+     dbms_output.put_line ('Invalid User');
+     end if;
+   END;
+    
+    --Procedure2 body
+    PROCEDURE procedure2 (productname varchar2 , companyname varchar2 ) AS
+    exist number(2);
+    productversion number(4,2);
+    today date;
+    BEGIN
+    select sysdate into today from dual;
+    select max(version) into productversion from products where name = procedure2.productname and cif = procedure2.companyname;
+    select count (*) into exist from products where name = procedure2.productname and cif = procedure2.companyname;
+    if exist != 0 and package1.curr_user is not null then
+    dbms_output.put_line (procedure2.productname);
+    dbms_output.put_line (procedure2.companyname);
+    dbms_output.put_line (productversion);
+    insert into policies (company, product, version, client, start_date, duration, recipients) values(procedure2.companyname, procedure2.productname, productversion, package1.curr_user, today, 30, 1);  
+    dbms_output.put_line ('Product correctly assigned');
+    else
+    dbms_output.put_line ('Invalid data');
+    end if;
+    END;
+    
+    --Procedure3 body
+    PROCEDURE procedure3 (specialty VARCHAR2, schedule DATE, doctor VARCHAR2, hospital VARCHAR2) AS
+    checkspecialty number (1);
+    checkdate number(1);
+    checkdochosp number(1);
+    BEGIN
+    select count(*) into checkspecialty from coverages where name = procedure2.productname AND cif = procedure2.companyname AND specialty = procedure3.specialty;
+    select start_date into chekdate from policies where product = procedure2.productname AND company = procedure2.companyname;
+    select count(*) into checkdochops from adscriptions where hospital = procedure3.hospital and specialty = procedure3.specialty and doctor = proocedure3.doctor
+    if 
+    If chekspecialty !=0 AND schedule > start_date then
+    select * from policies;
+    END;
+    
+    
+   END package1;
+   /
