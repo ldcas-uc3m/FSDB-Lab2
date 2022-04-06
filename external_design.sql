dced9cac18e3715438ee666a20b1a7396ed01dba
ldcas-uc3m <rajayonin.minecraft@hotmail.com>
2022-04-06 20:38:39 +0200

    i'm tired

--- external_design.sql
+++ external_design.sql
@@ -3,7 +3,7 @@
 -- --
 
 --CREATE OR REPLACE VIEW OVERLAPS  AS 
-  -- SELECT* FROM ( select * from products where (products.launch < sysdate and products.retired is null and user = PKG_users.getCurrentUser()))
+  -- SELECT* FROM ( select * from products where (products.launch < sysdate and products.retired is null and user = package1.getCurrentUser()))
    --JOIN COVERAGES 
    --using(CIF, name, version)
  --with read only;
@@ -40,33 +40,41 @@ CREATE OR REPLACE VIEW My_Coverages AS (
     FROM (
         SELECT company as cif, product as name, version 
         FROM Policies
-        WHERE ((SYSDATE - start_date < 1) AND (user = PKG_users.getCurrentUser()))
+        WHERE ((SYSDATE - start_date < 1) AND (client = package1.getCurrentUser()))
     ) JOIN (SELECT DISTINCT cif, name, version, specialty FROM Coverages)
         USING (cif, name, version)
     GROUP BY cif, name, version
 );
 --
--- test: 
--- insert into policies values ('15755166M', 'P?liza Sensacion', to_number('2.02', '9.99'), '13869622-R', (sysdate), 69, 1);
---
+
 -- This view will be operational, allowing the insertion of a row: if that product has that coverage, 
 -- what is inserted is a new policy for this user and that product, so that from now on the row will appear in this view 
 -- (the row inserted); it will also allow the deletion of a row (the policy will be deleted, so that the row will no longer 
 -- belong to the view); updates won’t have effect on this view (no change made).
 --
-CREATE OR REPLACE TRIGGER TG_Insert_My_Coverages
+CREATE OR REPLACE TRIGGER TG_Insert_My_Coverages -- does not work :(
     INSTEAD OF INSERT ON My_Coverages
     BEGIN
-    -- we assume you can try to insert (cif, product, version)
-        INSERT INTO Policies VALUES (
-            :NEW.cif,
-            :NEW.product,
-            :NEW.version,
-            PKG_users.getCurrentUser(), -- user
-            SYSDATE, -- start
-            365, -- duration, put default
-            1 -- recipients, default
-        );
+    -- we assume you can try to insert (cif, product, version, coverages)
+        IF :NEW.coverages IN (
+            SELECT specialty FROM Coverages
+            WHERE 
+                cif=:NEW.company
+                AND name=:NEW.product
+                AND version=:NEW.version
+        )
+        THEN
+            INSERT INTO Policies VALUES (
+                :NEW.company,
+                :NEW.product,
+                :NEW.version,
+                package1.getCurrentUser(), -- user
+                SYSDATE, -- start
+                365, -- duration, put default
+                1 -- recipients, default
+            );
+        ELSE DBMS_OUTPUT.PUT_LINE('Invalid coverage');
+        END IF;
     END;
     /
 --
@@ -75,13 +83,23 @@ CREATE OR REPLACE TRIGGER TG_Delete_My_Coverages
     BEGIN
         DELETE FROM Policies
         WHERE 
-            company=:OLD.cif,
-            AND product=:OLD.product,
-            AND version=:OLD.version,
-            AND user=PKG_users.getCurrentUser()
+            company=:OLD.company
+            AND product=:OLD.product
+            AND version=:OLD.version
+            AND client=package1.getCurrentUser()
         ;
     END;
     /
+
+-- test: 
+BEGIN
+    package1.procedure1('13869622-R');
+END;
+/
+INSERT INTO Policies VALUES ('15755166M', 'P?liza Sensacion', to_number('2.02', '9.99'), '13869622-R', (sysdate), 69, 1);
+SELECT * FROM My_Coverages;
+INSERT INTO My_Coverages VALUES ('02757650L', 'Seguro Balada y ella', to_number('3.02', '9.99'), 'Inmunolog?a');
+DELETE FROM My_Coverages WHERE company='02757650L' and product='Seguro Balada y ella' and version = to_number('3.02', '9.99');
 
 -- --
 -- VIEW 3
@@ -106,7 +124,7 @@ CREATE OR REPLACE VIEW Reccomendations AS (
             SELECT specialty FROM (
                 SELECT company AS cif, product AS name, version FROM Policies
                 WHERE 
-                    client=PKG_users.getCurrentUser()
+                    client=package1.getCurrentUser()
                     AND (start_date + duration > SYSDATE)
             ) JOIN Coverages USING (cif, name, version)
         )
